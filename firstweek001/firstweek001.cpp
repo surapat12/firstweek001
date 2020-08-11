b3e010401c3417a453842d044300b1182133307e
iamsurapat12 <iamsurapat12@outlook.co.th>
2020-08-11 13:46:10 +0700

    Add project files.

---
+++ firstweek001/firstweek001.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+	float Base, Height, Area;
+	printf("Enter Base :");
+	scanf_s ("%f", &Base);
+	printf("Enter Height :");
+	scanf_s ("%f", &Height);
+	Area = 0.5 * Base * Height;
+	printf("Area is : %f", Area);
+
+
+}
